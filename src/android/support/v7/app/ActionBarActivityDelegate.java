029dda13a2fe9834a6796491b6c22c6041ae640f
Adam Smialek <adam@getkeepsafe.com>
2014-08-25 14:40:15 -0700

    just check the model

--- src/android/support/v7/app/ActionBarActivityDelegate.java
+++ src/android/support/v7/app/ActionBarActivityDelegate.java
@@ -44,11 +44,11 @@ abstract class ActionBarActivityDelegate {
     private static final String TAG = "ActionBarActivityDelegate";
 
     static ActionBarActivityDelegate createDelegate(ActionBarActivity activity) {
-        String model = String.format("%s/%s", Build.DEVICE, Build.MODEL);
+        String model = Build.MODEL;
 
         //it would be great if manufacturers didn't ship phones with incomplete android APIs...
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
-                && (model.equals("ha3g/SM-N900") || model.equals("hlte/SM-N9006"))) {
+                && (model.equals("SM-N900") || model.equals("SM-N9006"))) {
             return new ActionBarActivityDelegateJB(activity);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             return new ActionBarActivityDelegateJBMR2(activity);
